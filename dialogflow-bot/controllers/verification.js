38113675cf069fd2aff861ea5a85c71b1686027f
ogambakerubo <ogambahilda@gmail.com>
2019-06-21 16:33:54 +0300

    Adopt-a-tree chat bot: use dialogflow
      - create a small talk chatbot
      - add a meaningful conversation flow

---
+++ dialogflow-bot/controllers/verification.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+
+module.exports = (req, res) => {
+  const hubChallenge = req.query["hub.challenge"];
+
+  const hubMode = req.query["hub.mode"];
+  const verifyTokenMatches = (req.query["hub.verify_token"] === process.env.VERIFY_TOKEN);
+
+  if (hubMode && verifyTokenMatches) {
+    res.status(200).send(hubChallenge);
+  } else {
+    res.status(403).end();
+  }
+};
---
+++ dialogflow-bot/helpers/processMessage.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const APIAI_TOKEN = process.env.API_AI_TOKEN;
+const apiAiClient = require('apiai')(APIAI_TOKEN);
+
+const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
+const request = require("request");
+
+const sendTextMessage = (senderId, text) => {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: FACEBOOK_PAGE_ACCESS_TOKEN,
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId,
+      },
+      message: {
+        text
+      },
+    }
+  });
+};
+
+module.exports = event => {
+  const senderId = event.sender.id;
+  const message = event.message.text;
+
+  const apiaiSession = apiAiClient.textRequest(message, {
+    sessionId: 'crowdbotics_bot'
+  });
+
+  apiaiSession.on("response", response => {
+    const result = response.result.fulfillment.speech;
+    sendTextMessage(senderId, result);
+  });
+
+  apiaiSession.on("error", error => console.log(error));
+  apiaiSession.end();
+};
